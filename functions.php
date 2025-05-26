192cdea1df3f345863da15e9a03eece6f3abaeaf
Gustavo Martínez <gusmbox@gmail.com>
2025-05-26 14:23:22 +0200

    added bootstrap

--- functions.php
+++ functions.php
@@ -15,5 +15,16 @@ function mi_tema_registrar_estilos() {
 }
 add_action('wp_enqueue_scripts', 'mi_tema_registrar_estilos');
 
+function mi_cargar_bootstrap() {
+    $base_url = get_stylesheet_directory_uri() . '/lib/bootstrap/';
+
+    // CSS
+    wp_enqueue_style('bootstrap-css', $base_url . 'bootstrap.min.css', array(), '5.3.3'); // Cambia versión si quieres
+
+    // JS - jQuery como dependencia para Bootstrap 4, para Bootstrap 5 no depende de jQuery
+    wp_enqueue_script('bootstrap-js', $base_url . 'bootstrap.min.js', array(), '5.3.3', true);
+}
+add_action('wp_enqueue_scripts', 'mi_cargar_bootstrap');
+
 ?>
 
