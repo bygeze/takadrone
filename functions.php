d190ba29f6a9fa3761792a559a6e64f07bd6b0d4
Gustavo Martínez <gusmbox@gmail.com>
2025-05-26 15:04:18 +0200

    added landing

--- functions.php
+++ functions.php
@@ -12,6 +12,12 @@ function mi_tema_registrar_estilos() {
         array(),
         null
     );
+    wp_register_style(
+        'landing',
+        get_template_directory_uri() . '/css/landing.css',
+        array(),
+        null
+    );
 }
 add_action('wp_enqueue_scripts', 'mi_tema_registrar_estilos');
 
---
+++ landing.php
@@ -0,0 +1,19 @@
+
+<div id="landing">
+        <video autoplay muted loop id="landing-video">
+            <source src="./assets/shot-intro.mp4" type="video/mp4">
+        </video>
+
+        <div id="video-overlay"></div>
+
+        <div id="center-container">
+            <div id="centered-div">
+                <h1>Deja volar tu imaginación<br>
+                    y dale alas a tu proyecto
+                </h1>
+                <div id="separator1"></div>
+                <h2>Servicios de filmación aeréa</h2>
+            </div>
+
+        </div>
+</div>
