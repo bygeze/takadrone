24e91d269127bcee9bb45196cf6bed9fa8d130c3
Gustavo Martínez <gusmbox@gmail.com>
2025-06-12 23:37:42 +0200

    services section added

---
+++ services.php
@@ -0,0 +1,35 @@
+    <div id="services">
+        <div class="container">
+            <h1>Servicios</h1>
+            <p>
+                Realizamos trabajos tanto en interior como en exterior con equipos adaptados a cada necesidad.<br>
+                Cuentanos tu proyecto y nos adaptaremos a ti cumpliendo objetivos y expectativas.<br><br>
+            </p>
+            <div id="services-flexbox">
+                <div class="services-fb-item">
+                    <div class="services-fb-item-icon">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/bombilla.png" alt="">
+                    </div>
+                    <span class="services-item-title">Explicanos tu idea</span><br>
+                    Cuéntanos tu proyecto y tipo de tomas necesarias.
+                </div>
+                <div class="services-fb-item">
+                    <div class="services-fb-item-icon">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/lista-de-verificacion.png" alt="">
+                    </div>
+                    <span class="services-item-title">Gestión y programación</span>
+                    <br>
+                    Despues de confirmar los detalles y requerimientos, coordinaremos la operación.
+                </div>
+                <div class="services-fb-item">
+                    <div class="services-fb-item-icon">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/entrega-de-paquetes.png" alt="">
+                    </div>
+                    <span class="services-item-title">Entrega de proyecto</span>
+                    <br>
+                    El plazo de entrega se realiza el mismo dia del vuelo si no requiere edición.​
+                </div>
+            </div>
+
+        </div>
+    </div>
