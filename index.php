616a7d52cfa7b1c2f75b15587b20549f10efd4ef
Gustavo Martínez <gusmbox@gmail.com>
2025-06-12 23:47:55 +0200

    where added to functions

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 <?php wp_enqueue_style('services'); ?>
 <?php get_template_part('services'); ?>
 
+<?php wp_enqueue_style('where'); ?>
+<?php get_template_part('where'); ?>
+
 <?php wp_enqueue_style('portfolio-display'); ?>
 <?php get_template_part('portfolio-display'); ?>
 
