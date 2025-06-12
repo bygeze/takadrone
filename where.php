632613a485b70fe3d78c97d502cab518183cd942
Gustavo Martínez <gusmbox@gmail.com>
2025-06-12 23:46:45 +0200

    css for srvices and where section added

---
+++ where.php
@@ -0,0 +1,19 @@
+    <div id="where">
+            <div id="center-container2">
+                <div id="centered-div">
+
+                    Volamos en todo el territorio Nacional y Europeo.<br>
+                    Según el espacio aéreo sea controlado o no, debemos hacer ciertas coordinaciones para poder operar por motivos de legislación y seguridad.<br>
+                    Dichos permisos suelen demorarse unos 10 a 15 dias hábiles.<br><br>
+                    A continuación, dejamos el enlace al mapa nacional de Enaire que indica las limitaciones en toda España.<br>
+                    https://drones.enaire.es/
+                </div>
+            </div>
+            <div id="video-overlay2">
+            </div>
+            <div id="where-video">
+                <video autoplay muted loop id="map-video">
+                    <source src="<?php echo get_template_directory_uri(); ?>/assets/map.mp4" type="video/mp4">
+                </video>
+            </div>
+    </div>
